d31978db083f704e157fdf4dbe5b6f3e7cf7b8b1
Shawn Huggins <hugginsjrshawn@gmail.com>
2019-09-21 18:10:52 -0500

    Add login and signup functionality without sessions.

--- src/server/migrations/001-init.sql
+++ src/server/migrations/001-init.sql
@@ -4,21 +4,21 @@
 
 CREATE TABLE IF NOT EXISTS user 
   ( 
-     id            INTEGER PRIMARY KEY, 
-     NAME          STRING UNIQUE NOT NULL, 
-     password_hash STRING NOT NULL
+     id                INTEGER PRIMARY KEY AUTOINCREMENT, 
+     username          STRING UNIQUE NOT NULL, 
+     password_hash     STRING NOT NULL
   ); 
 
 CREATE TABLE IF NOT EXISTS feed 
   ( 
-     id         INTEGER PRIMARY KEY, 
-     NAME       STRING NOT NULL, 
+     id         INTEGER PRIMARY KEY AUTOINCREMENT, 
+     name       STRING NOT NULL, 
      url        STRING NOT NULL, 
      fk_user_id INTEGER NOT NULL,
      FOREIGN KEY (fk_user_id) REFERENCES user (id)
   );
 
-  INSERT INTO user VALUES (0, "test", "test");
+  INSERT INTO user VALUES (0, "test", "$2b$10$EPn0cAXze8Y.NgHt.xuwBOX09lCOiK3LQBxe4LRqXG75sgy9am4.K");
   INSERT INTO feed VALUES (0, "test", "test", 0);
 
 --------------------------------------------------------------------------------
--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const sqlite = require('sqlite')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt');
 const app = express()
 const PORT = 8000
+const saltRounds = 10;
 
 const dbPromise = Promise.resolve()
     .then(() => sqlite.open('./database.sqlite', { Promise }))
@@ -10,7 +13,48 @@ const dbPromise = Promise.resolve()
         console.log(`Server could not start: ${error}`)
     })
 
+sql = {
+    findByUserName: 'SELECT * FROM user WHERE username = ?',
+    allUserFeeds: 'SELECT id, name, url FROM feed WHERE fk_user_id = ?',
+    insertUser: 'INSERT INTO user (username, password_hash) VALUES (?, ?)'
+}
+
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/login', async (req, res, next) => {
+    try {
+        const db = await dbPromise
+        const user = await db.get(sql.findByUserName, req.body.user.username)
+        if (!user) {
+            throw "username or password is incorrect"
+        }
+        const passwordsEqual = await bcrypt.compare(req.body.user.password, user.password_hash)
+        if (!passwordsEqual) {
+            throw 'Username or password is incorrect'
+        }
+        let feeds = await db.get(sql.allUserFeeds, user.id)
+        feeds = feeds ? feeds : []
+        res.send({ username: user.username, feeds: feeds })
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.post('/signup', async (req, res, next) => {
+    try {
+        const db = await dbPromise
+        const passwordHash = await bcrypt.hash(req.body.user.password, saltRounds)
+        await db.run(sql.insertUser, req.body.user.username, passwordHash)
+        res.send({
+            username: req.body.user.username,
+            feeds: []
+        })
+    } catch (error) {
+        next(error)
+    }
+})
 
 /*
 * A simple function to test interfacing with SQLite.
