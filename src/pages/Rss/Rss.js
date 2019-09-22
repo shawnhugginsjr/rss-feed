17b63a2f669d7a078b135374da92f325c68a8f1a
Shawn Huggins <hugginsjrshawn@gmail.com>
2019-09-22 16:23:10 -0500

    Can display feed text in browser.

--- src/pages/Rss/Rss.js
+++ src/pages/Rss/Rss.js
@@ -1,15 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 const queryString = require('query-string')
 
 export function Rss({ location, history }) {
+  const [feed, saveFeed] = useState(null)
+  let feedText = ''
+
   useEffect(() => {
     const queryStrings = queryString.parse(location.search)
-    if (queryStrings.feedUrl === undefined) {
+    if (!queryStrings.feedUrl) {
       history.replace('/')
+    }
+
+    if (!feed) {
+      fetch(`/rss?feedUrl=${queryStrings.feedUrl}`)
+        .then((res) => res.json())
+        .then((jsonFeed) => saveFeed(jsonFeed))
+        .catch((error) => console.log(error))
     }
   })
 
+  if (feed) {
+    feedText = JSON.stringify(feed)
+  } 
+
   return (
-    <div>Rss page</div>
+    <div>
+      <p>{feedText}</p>
+    </div>
   )
 }
